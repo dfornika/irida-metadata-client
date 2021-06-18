9a9cf4c7a42e240cb524f006b58dc6d4789982df
Dan Fornika <dfornika@gmail.com>
2021-06-18 12:53:57 -0700

    produce csv output

--- irida_metadata_client.py
+++ irida_metadata_client.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 import argparse
+import csv
 import json
+import sys
 
 from rauth import OAuth2Service
 from urllib.parse import urlsplit
@@ -28,13 +30,13 @@ def join_path(base_path, path):
             return base_path + '/' + path
 
 
-def get(session, base_path, path):
+def get(session, path):
         """
         A GET request to a particular path in IRIDA.
         :param path: The path to GET, minus the IRIDA url (e.g., '/projects').
         :return:  The result of rauth.OAuth2Service.get()
         """
-        path = join_path(base_path, path)
+        # path = join_path(base_path, path)
 
         response = session.get(path, timeout=10)
 
@@ -70,10 +72,30 @@ def main(args):
 
     session = oauth_service.get_session(token)
 
-    project = get(session, base_url, '/projects/' + args.project_id)
-    project_samples = get(session, base_url, '/projects/' + args.project_id + '/samples')['resources']
-    print(json.dumps(project_samples, indent=2))
-    
+    project = get(session, join_path(base_url, '/projects/' + args.project_id))
+    project_samples = get(session, join_path(base_url, '/projects/' + args.project_id + '/samples'))['resources']
+
+    samples_with_metadata = []
+    all_metadata_fields = set()
+    for sample in project_samples:
+        s = {}
+        s['sample_irida_id'] = sample['identifier']
+        s['sample_name'] = sample['sampleName']
+        metadata_link = list(filter(lambda x: x['rel'] == 'sample/metadata', sample['links']))[0]['href']
+        sample_metadata = get(session, metadata_link)['metadata']
+        for k, v in sample_metadata.items():
+            all_metadata_fields.add(k)
+            s[k] = v['value']
+        samples_with_metadata.append(s)
+
+
+    output_fieldnames = ['sample_irida_id', 'sample_name'] + list(all_metadata_fields)
+    writer = csv.DictWriter(sys.stdout, fieldnames=output_fieldnames)
+    writer.writeheader()
+    for sample in samples_with_metadata:
+        writer.writerow(sample)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("irida_metadata_client.py")
     parser.add_argument('--base-url', help="")
